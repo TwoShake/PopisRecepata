797025bf95c36b26b9b2f48662612cd3aedae7f2
TwoShake <cod5wiw@gmail.com>
2020-11-05 20:53:42 +0100

    initialize

---
+++ src/components/navigacija.vue
@@ -0,0 +1,32 @@
+<template>
+    <b-navbar>
+        <template slot="start">
+            <b-navbar-item href="#">
+                Svaštara
+            </b-navbar-item>
+            <b-navbar-item href="#">
+            Slastice
+            </b-navbar-item>
+            <b-navbar-item href="#">
+            Pića
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
+<script>
+export default {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.navbar{
+    padding-top: 50px;
+    background-color: black;
+}
+b-navbar-item{
+    padding:12px;
+    margin-left:20px;
+    font-size:40px;
+    color:white;
+}
+</style>
---
+++ src/components/svastara.vue
@@ -0,0 +1,22 @@
+<template>
+    <b-navbar>
+        <template slot="start">
+            <b-navbar-item href="#">
+                Svaštara
+            </b-navbar-item>
+            <b-navbar-item href="#">
+            Slastice
+            </b-navbar-item>
+            <b-navbar-item href="#">
+            Pića
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
+<script>
+export default {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
